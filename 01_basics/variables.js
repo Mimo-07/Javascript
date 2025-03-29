4c174319a5a45f6bd70d9c849f2915bf213a210b
Mimo-07 <59221049+Mimo-07@users.noreply.github.com>
2025-03-29 07:42:11 +0000

    variables let,const,var

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 14453;
+
+let accountEmail = "sample@email.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 12345;
+
+accountEmail = "12@tu.com";
+accountPassword = "76456";
+accountCity = "Kolkata"
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
